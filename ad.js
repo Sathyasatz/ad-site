8ca1bf0b8539fea2b77f609d0003e40833619a61
Sathiya Naarayanan <bals@healthstandards.org>
2023-10-13 16:22:07 -0400

    Code replace

--- ad.js
+++ ad.js
@@ -15,7 +15,7 @@ app.get('/banner', function (req, res) {
     hits++;
     // res.cookie("third-party", hits.toString(), { maxAge: 9999999 });
     if (!req.cookies['third-party']) {
-        res.setHeader('Set-Cookie',  'third-party=' + hits.toString());
+        res.setHeader('Set-Cookie',  'third-party=' + hits.toString() + '; SameSite=None; ');
         // res.cookie("third-party", hits.toString(), { maxAge: 9999999 });
     }
     else{
