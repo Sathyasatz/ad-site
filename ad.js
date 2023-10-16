b3fe58c7f53fa263ac7d89880506f7aaa5f215f5
Sathiya Naarayanan <bals@healthstandards.org>
2023-10-16 13:03:27 -0400

    header

--- ad.js
+++ ad.js
@@ -15,8 +15,8 @@ app.get('/banner', function (req, res) {
     hits++;
     // res.cookie("third-party", hits.toString(), { maxAge: 9999999 });
     if (!req.cookies['third-party']) {
-        // res.setHeader('Set-Cookie',  'third-party=' + hits.toString() + '; SameSite=None; Secure; Path=/;');
-        res.cookie("third-party", hits.toString(), { maxAge: 9999999, httpOnly: true , sameSite:'none', secure:true});
+        res.setHeader('Set-Cookie',  'third-party=' + hits.toString() + '; SameSite=None; Secure; Path=/;');
+        // res.cookie("third-party", hits.toString(), { maxAge: 9999999, httpOnly: true , sameSite:'none', secure:true});
     }
     else{
         console.log(req.cookies);
